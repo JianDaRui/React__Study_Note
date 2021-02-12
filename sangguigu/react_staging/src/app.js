1b1e86888b2cf0a05dbaabe5a0f887a1d32fc2fe
AmosRui <1343843253@qq.com>
2021-02-12 17:57:48 +0800

    p

--- sangguigu/react_staging/src/app.js
+++ sangguigu/react_staging/src/app.js
@@ -3,77 +3,12 @@ import React, { Component } from 'react';
 
 // import TODOList from './TODOList'
 import './app.css'
-import Header from './components/Header/Header';
-import List from './components/List/List';
-import Footer from './components/Footer/Footer';
+
 class App extends Component{
-  constructor() {
-    super()
-    this.state = {
-      todos:[
-        {id: '001', name: '吃饭', done: false},
-        {id: '002', name: '工作', done: false},
-        {id: '003', name: '学习', done: true},
-        {id: '004', name: '锻炼', done: false},
-      ]
-    }
-  }
-  addTodo = (todoObj) => {
-    let { todos } = this.state
-    console.log(todoObj, '-----')
-    this.setState({
-      todos: [todoObj, ...todos]
-    })
-  }
-  updateToDo = (id, done) => {
-    const { todos } = this.state
-    let newTodo = todos.map(todoObj => {
-      if(todoObj.id === id) {
-        return { ...todoObj, done }
-      } else {
-        return todoObj
-      }
-    })
-    this.setState({
-      todos: newTodo
-    })
-  }
-  deleteTodo = (id) => {
-    const { todos } = this.state
-    let newTodo = todos.filter(item => {
-      return item.id !== id
-    })
-    this.setState({
-      todos: newTodo
-    })
-  }
-  checkToDoAll = (done) => {
-    const { todos} = this.state;
-    const newTodo = todos.map((todoObj) => {
-      return {...todoObj, done: done }
-    })
-    this.setState({
-      todos: newTodo
-    })
-  }
-  handleDeleteAll = () => {
-    const {todos} = this.state;
-    const newTodo = todos.filter(item => {
-      return !item.done 
-    })
-    this.setState({
-      todos: newTodo
-    })
-  }
   render() {
-    const { todos } = this.state
     return (
-      <div className="todo-container">
-        <div className="todo-wrap">
-          <Header addTodo={ this.addTodo } />
-          <List todos={ todos } updateToDo={ this.updateToDo } deleteTodo={this.deleteTodo}/>
-          <Footer todos={ todos } checkToDoAll={ this.checkToDoAll } handleDeleteAll={ this.handleDeleteAll }/>
-        </div>
+      <div >
+        
       </div>
     )
   }
