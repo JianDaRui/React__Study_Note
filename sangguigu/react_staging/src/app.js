6183e64c368647454291609d7f2e46547c3c8a9f
AmosRui <1343843253@qq.com>
2021-02-12 18:38:46 +0800

    p

--- sangguigu/react_staging/src/app.js
+++ sangguigu/react_staging/src/app.js
@@ -1,14 +1,19 @@
 
 import React, { Component } from 'react';
-
+import axios from 'axios'
 // import TODOList from './TODOList'
 import './app.css'
-
 class App extends Component{
+  getData = () => {
+    axios.get().then(
+      response => {console.log("成功了", response.data)},
+      error => {console.log('失败了', error)}
+    )
+  }
   render() {
     return (
       <div>
-        App
+        <button onClick={this.getData}>过去数据</button>
       </div>
     )
   }
