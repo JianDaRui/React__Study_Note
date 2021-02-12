1311b7357ed4da1669b485ad280457e0f116b1cf
AmosRui <1343843253@qq.com>
2021-02-12 21:41:05 +0800

    p

--- sangguigu/react_staging/src/components/Search/index.jsx
+++ sangguigu/react_staging/src/components/Search/index.jsx
@@ -5,19 +5,48 @@ import axios from 'axios';
 import './index.css'
 
 export default class Search extends Component {
-  search = () => {
+  constructor(params) {
+    // search = search.bind(this)
+  }
+  search = async() => {
     
     const { keyWordElement: {value} } = this;
     PubSub.publish('state', { isFirst: false, isLoading: true })
-    axios.get(`/api1/search/users2?q=${value}`).then(
-      success => {
-        console.log('success', success) 
-        PubSub.publish('state', { isLoading: false, users: success.data.items })
-      },
-      error => { 
-        PubSub.publish('state', { isLoading: false, error: error.message })
-      }
-    )
+    // axios.get(`/api1/search/users2?q=${value}`).then(
+    //   success => {
+    //     console.log('success', success) 
+    //     PubSub.publish('state', { isLoading: false, users: success.data.items })
+    //   },
+    //   error => { 
+    //     PubSub.publish('state', { isLoading: false, error: error.message })
+    //   }
+    // )
+    // fetch(`/api1/search/users2?q=${value}`).then(
+    //   response => {
+    //     console.log('success', response.json()) 
+    //     return response.json()
+    //     // PubSub.publish('state', { isLoading: false, users: success.data.items })
+    //   }
+    //   // error => { 
+    //   //   return new Promise(() => {})
+    //   //   // PubSub.publish('state', { isLoading: false, error: error.message })
+    //   // }
+    // ).then(
+    //   response => {console.log('获取数据成功', response)},
+    //   error => {console.log('获取数据失败')}
+    // ).catch(err => {
+    //   console.log(err)
+    // })
+
+    try {
+			const response= await fetch(`/api1/search/users2?q=${keyWord}`)
+			const data = await response.json()
+			console.log(data);
+			PubSub.publish('atguigu',{isLoading:false,users:data.items})
+		} catch (error) {
+			console.log('请求出错',error);
+			PubSub.publish('atguigu',{isLoading:false,err:error.message})
+		}
   }
   render() {
     return (
