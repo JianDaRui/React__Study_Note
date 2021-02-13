b059d982a301d899de092b8f16e8e44d5d46edf0
AmosRui <1343843253@qq.com>
2021-02-13 16:23:13 +0800

    history编程式道行

---
+++ sangguigu/react_staging/20_编程式道行/pages/About/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  constructor(props) {
+    super(props)
+    // console.log(props)
+  }
+  render() {
+    return (
+      <div>
+        <h3>我是About的内容</h3>
+      </div>
+    )
+  }
+}
