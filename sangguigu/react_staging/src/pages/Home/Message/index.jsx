7b63b826d5ce83e69fb47772e8507ce3dc3d2102
AmosRui <1343843253@qq.com>
2021-02-13 16:19:57 +0800

    p

--- sangguigu/react_staging/src/pages/Home/Message/index.jsx
+++ sangguigu/react_staging/src/pages/Home/Message/index.jsx
@@ -12,6 +12,40 @@ export default class Message extends Component {
       ]
     }
   }
+  replaceShow = (id,title)=>{
+		//replace跳转+携带params参数
+		//this.props.history.replace(`/home/message/detail/${id}/${title}`)
+
+		//replace跳转+携带search参数
+		// this.props.history.replace(`/home/message/detail?id=${id}&title=${title}`)
+
+		//replace跳转+携带state参数
+		this.props.history.replace(`/home/message/detail`,{id,title})
+	}
+
+	pushShow = (id,title)=>{
+		//push跳转+携带params参数
+		// this.props.history.push(`/home/message/detail/${id}/${title}`)
+
+		//push跳转+携带search参数
+		// this.props.history.push(`/home/message/detail?id=${id}&title=${title}`)
+
+		//push跳转+携带state参数
+		this.props.history.push(`/home/message/detail`,{id,title})
+		
+	}
+
+	back = ()=>{
+		this.props.history.goBack()
+	}
+
+	forward = ()=>{
+		this.props.history.goForward()
+	}
+
+	go = ()=>{
+		this.props.history.go(-2)
+	}
   render() {
     const { messageArr } = this.state
     return (
@@ -27,6 +61,8 @@ export default class Message extends Component {
                   {/* <Link to={`/home/message/detail/?id=${item.id}&title=${item.title}`}>{ item.title } </Link>&nbsp;&nbsp; */}
                   {/* 向路由组件传递state参数 */}
                   <Link replace to={{pathname:`/home/message/detail`, state: { id:item.id, title: item.title }}}>{ item.title } </Link>&nbsp;&nbsp;
+                  &nbsp;<button onClick={()=> this.pushShow(item.id,item.title)}>push查看</button>
+									&nbsp;<button onClick={()=> this.replaceShow(item.id,item.title)}>replace查看</button>
                 </li>
               )
             })
@@ -38,6 +74,9 @@ export default class Message extends Component {
           
         {/* 声明接收search参数, 正常注册即可*/}
         <Route path="/home/message/detail" component={Detail}></Route>
+        <button onClick={this.back}>回退</button>&nbsp;
+				<button onClick={this.forward}>前进</button>&nbsp;
+				<button onClick={this.go}>go</button>
       </div>
     )
   }
