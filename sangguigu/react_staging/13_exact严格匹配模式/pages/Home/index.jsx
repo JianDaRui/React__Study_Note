050a3bebd6d08360ee663b39e3cc2b1ee2d27e34
AmosRui <1343843253@qq.com>
2021-02-13 12:04:01 +0800

    p

---
+++ sangguigu/react_staging/13_exact严格匹配模式/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props)
+    // console.log(props)
+  }
+  render() {
+    return (
+      <div>
+        <h3>我是Home的内容</h3>
+      </div>
+    )
+  }
+}
