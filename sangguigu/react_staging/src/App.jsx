d867f24a6501ec36fc578eec2349c413309d27ba
AmosRui <1343843253@qq.com>
2021-02-13 17:31:18 +0800

    自定义主题

--- sangguigu/react_staging/src/App.jsx
+++ sangguigu/react_staging/src/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-
+import { Button } from 'antd';
+import { WechatOutlined, SearchOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css'
 export default class App extends Component {
   render() {
     return (
       <div>
-        App
+        App...
+        <Button type="primary" > Primary Button </Button>
+        <WechatOutlined />
+        <Button icon={<SearchOutlined />}>Search</Button>
+    <br />
       </div>
     )
   }
