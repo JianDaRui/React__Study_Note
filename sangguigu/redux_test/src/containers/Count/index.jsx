423750ee08c201c79d0423b8261e5b648c893fae
AmosRui <1343843253@qq.com>
2021-02-14 10:13:13 +0800

    优化redux

--- sangguigu/redux_test/src/containers/Count/index.jsx
+++ sangguigu/redux_test/src/containers/Count/index.jsx
@@ -15,22 +15,18 @@ import {connect} from 'react-redux'
 	2.返回的对象中的key就作为传递给UI组件props的key,value就作为传递给UI组件props的value
 	3.mapStateToProps用于传递状态
 */
-function mapStateToProps(state){
-	return {count:state}
-}
+const mapStateToProps = (state) => ({count:state})
 
 /* 
 	1.mapDispatchToProps函数返回的是一个对象；
 	2.返回的对象中的key就作为传递给UI组件props的key,value就作为传递给UI组件props的value
 	3.mapDispatchToProps用于传递操作状态的方法
 */
-function mapDispatchToProps(dispatch){
-	return {
-		jia:number => dispatch(createIncrementAction(number)),
-		jian:number => dispatch(createDecrementAction(number)),
-		jiaAsync:(number,time) => dispatch(createIncrementAsyncAction(number,time)),
-	}
-}
+const mapDispatchToProps = (dispatch) => ({
+	jia:number => dispatch(createIncrementAction(number)),
+	jian:number => dispatch(createDecrementAction(number)),
+	jiaAsync:(number,time) => dispatch(createIncrementAsyncAction(number,time)),
+})
 
 //使用connect()()创建并暴露一个Count的容器组件
 export default connect(mapStateToProps,mapDispatchToProps)(CountUI)
