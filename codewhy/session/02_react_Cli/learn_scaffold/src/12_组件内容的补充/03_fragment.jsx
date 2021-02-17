da667b9b3c05b15ab1a7c5dc5e931bc657ed4a1a
AmosRui <1343843253@qq.com>
2021-02-17 15:59:09 +0800

    p

---
+++ codewhy/session/02_react_Cli/learn_scaffold/src/12_组件内容的补充/03_fragment.jsx
@@ -0,0 +1,38 @@
+import React, { PureComponent, Fragment } from 'react'
+
+class Demo extends PureComponent {
+  render() {
+    return (
+      /* 短语法 */
+      // 如果遍历时 添加key属性 短语法不可以添加任何属性
+      <>
+        <div>
+          Fragment
+        </div>
+        <div>
+          Fragment
+        </div>
+      </>
+    )
+  }
+}
+// Fragment 可以解决 只有一个跟标签的问题
+export default class App extends PureComponent {
+  render() {
+    return (
+      <Fragment>
+        
+        <Demo />
+        <div>
+          Fragment
+        </div>
+        <div>
+          Fragment
+        </div>
+        <div>
+          Fragment
+        </div>
+      </Fragment>
+    )
+  }
+}
--- codewhy/session/02_react_Cli/learn_scaffold/src/index.js
+++ codewhy/session/02_react_Cli/learn_scaffold/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 // import App from './11_高阶组件的使用/App'
-import App from './12_组件内容的补充/02_portals的使用'
+// import App from './12_组件内容的补充/02_portals的使用'
+import App from './12_组件内容的补充/03_fragment'
 ReactDOM.render(< App /> , document.querySelector('#root'))
