9ba0eed0e1be28f4549b0e1b9fda7dd8bb1bc894
JianDaRui <1343843253@qq.com>
2021-09-28 19:33:09 +0800

    router

--- 我的学习笔记/06_React脚手架/10_react-redux/src/store/actionCreators.js
+++ 我的学习笔记/06_React脚手架/10_react-redux/src/store/actionCreators.js
@@ -1,8 +1,13 @@
+import axios from 'axios';
+
 import {
   ADD_NUMBER,
   SUB_NUMBER,
   INCREMENT,
-  DECREMENT
+  DECREMENT,
+  CHANGE_BANNERS,
+  CHANGE_RECOMMEND,
+  FETCH_HOME_MULTIDATA
 } from './constants.js';
 
 // export function addAction(num) {
@@ -36,4 +41,34 @@ export const incAction = () => ({
 export const decAction = () => ({
   type: DECREMENT
 });
+
+
+// 轮播图和推荐的action
+export const changeBannersAction = (banners) => ({
+  type: CHANGE_BANNERS,
+  banners
+});
+
+export const changeRecommendAction = (recommends) => ({
+  type: CHANGE_RECOMMEND,
+  recommends
+});
+
+
+// redux-thunk中定义的action函数
+export const getHomeMultidataAction = (dispatch, getState) => {
+  axios({
+    url: "http://123.207.32.32:8000/home/multidata",
+  }).then(res => {
+    const data = res.data.data;
+    dispatch(changeBannersAction(data.banner.list));
+    dispatch(changeRecommendAction(data.recommend.list));
+  })
+}
+
+
+// redux-saga拦截的action
+export const fetchHomeMultidataAction = {
+  type: FETCH_HOME_MULTIDATA
+}
 
