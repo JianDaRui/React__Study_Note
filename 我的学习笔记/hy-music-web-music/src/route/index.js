951a8e8cca814da4637b5da015616028e368e968
AmosRui <1343843253@qq.com>
2021-10-19 21:31:04 +0800

    discover 子页面

--- 我的学习笔记/hy-music-web-music/src/route/index.js
+++ 我的学习笔记/hy-music-web-music/src/route/index.js
@@ -1,5 +1,12 @@
-
+import { Redirect } from 'react-router-dom';
 import HYDiscover from '../pages/discover/index';
+import HYAlbum from '../pages/discover/child-pages/album'
+import HYArtist from '../pages/discover/child-pages/artist'
+import HYRecommend from '../pages/discover/child-pages/recommend'
+import HYSongs from '../pages/discover/child-pages/songs'
+import HYDjradio from '../pages/discover/child-pages/djradio'
+import HYRanking from '../pages/discover/child-pages/ranking'
+
 import HYFriends from '../pages/friends/index';
 import HYPlayer from '../pages/player/index';
 import HYMime from '../pages/mime/index';
@@ -8,11 +15,50 @@ const routes = [
   {
     path: "/",
     exact: true,
-    component: HYDiscover
+    render: () => (
+      <Redirect to="/discover" />
+    )
   },{
     path: "/discover",
-    exact: true,
-    component: HYDiscover
+    component: HYDiscover,
+    routes: [
+      {
+        path: "/discover",
+        exact: true,
+        render: () => (
+          <Redirect to="/discover/recommend"/>
+        )
+      },
+      {
+        path: "/discover/recommend",
+        component: HYRecommend
+      },
+      {
+        path: "/discover/ranking",
+        component: HYRanking
+      },
+      {
+        path: "/discover/songs",
+        component: HYSongs
+      },
+      {
+        path: "/discover/djradio",
+        exact: true,
+        component: HYDjradio
+      },
+      {
+        path: "/discover/artist",
+        component: HYArtist
+      },
+      {
+        path: "/discover/album",
+        component: HYAlbum
+      },
+      // {
+      //   path: "/discover/player",
+      //   component: HYPlayer
+      // }
+    ]
   },{
     path: "/friends",
     exact: true,
