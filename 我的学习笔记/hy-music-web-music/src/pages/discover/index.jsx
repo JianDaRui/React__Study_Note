2c16d67a0d225606d8e1c9cd9259e32f43795e42
AmosRui <1343843253@qq.com>
2021-10-19 21:38:46 +0800

    引入axios

--- 我的学习笔记/hy-music-web-music/src/pages/discover/index.jsx
+++ 我的学习笔记/hy-music-web-music/src/pages/discover/index.jsx
@@ -1,14 +1,27 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import { dicoverMenu } from '../../common/local-data'
-
+import request from '../../services/request'
 import { 
   DiscoverWrapper,
   TopMenu
 } from './style'
+
 export default memo(function HYDiscover(props) {
   const { route } = props
+
+  useEffect(() => {
+    request({
+      url: "/banner"
+    }).then(res => {
+      console.log(res);
+      
+    })
+    return () => {
+      
+    }
+  }, [])
   return (
     <DiscoverWrapper>
       <div className="top">
