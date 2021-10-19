61eb8bd78ab7b55094385eb6eb83413d5f999c52
AmosRui <1343843253@qq.com>
2021-10-19 23:10:21 +0800

    了解数据的不可变性

--- 我的学习笔记/hy-music-web-music/src/pages/discover/child-pages/recommend/index.jsx
+++ 我的学习笔记/hy-music-web-music/src/pages/discover/child-pages/recommend/index.jsx
@@ -1,5 +1,5 @@
 import React, { memo, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector, shallowEqual } from 'react-redux'
 import { getTopBannerAction } from './store/actionCreators'
 
 function HYRecommend(props) {
