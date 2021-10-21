373488be31e22b4b5cd2af37e2a7b821de4a4107
AmosRui <1343843253@qq.com>
2021-10-21 21:59:52 +0800

    feat: 排行榜

--- 我的学习笔记/hy-music-web-music/src/pages/discover/child-pages/recommend/c-cpns/ranking/index.jsx
+++ 我的学习笔记/hy-music-web-music/src/pages/discover/child-pages/recommend/c-cpns/ranking/index.jsx
@@ -1,11 +1,35 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 
-import HYThemeHeader from '@/components/theme-header'
+import HYThemeHeader from '@/components/theme-header';
+import HYTopRanking from '@/components/top-ranking';
+import { RankingWrapper } from './style';
+import { getTopListAction } from '../../store/actionCreators';
 
-export default memo(function HYRecommendRanking() {
+export default memo(function HYRecomendRanking() {
+  // redux hooks
+  const { upRanking, newRanking, originRanking } = useSelector(state => ({
+    upRanking: state.getIn(["recommend", "upRanking"]),
+    newRanking: state.getIn(["recommend", "newRanking"]),
+    originRanking: state.getIn(["recommend", "originRanking"]),
+  }), shallowEqual);
+  const dispatch = useDispatch();
+
+  // other hooks
+  useEffect(() => {
+    dispatch(getTopListAction(0));
+    dispatch(getTopListAction(2));
+    dispatch(getTopListAction(3));
+  }, [dispatch]);
+
   return (
-    <div>
-      <HYThemeHeader title="排名"></HYThemeHeader>
-    </div>
+    <RankingWrapper>
+      <HYThemeHeader title="榜单" />
+      <div className="tops">
+        <HYTopRanking info={upRanking}/>
+        <HYTopRanking info={newRanking}/>
+        <HYTopRanking info={originRanking}/>
+      </div>
+    </RankingWrapper>
   )
 })
--- 我的学习笔记/hy-music-web-music/src/pages/discover/child-pages/recommend/store/actionCreators.js
+++ 我的学习笔记/hy-music-web-music/src/pages/discover/child-pages/recommend/store/actionCreators.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './constants';
-import { getTopBanners, getHotRecommends, getNewAlbums } from '@/services/recommend'
+import { getTopBanners, getHotRecommends, getNewAlbums, getTopList } from '@/services/recommend'
 export const getTopBannerAction = () => {
   return dispatch => {
     getTopBanners().then(res => {
@@ -41,3 +41,37 @@ const changeNewAlbumsAction = (res) => ({
   type: actionTypes.CHANGE_NEW_ALBUMS,
   newAlbums: res.albums
 })
+
+const changeUpRankingAction = (res) => ({
+  type: actionTypes.CHANGE_UP_RANKING,
+  upRanking: res.playlist
+})
+
+const changeNewRankingAction = (res) => ({
+  type: actionTypes.CHANGE_NEW_RANKING,
+  newRanking: res.playlist
+})
+
+const changeOriginRankingAction = (res) => ({
+  type: actionTypes.CHANGE_ORIGIN_RANKING,
+  originRanking: res.playlist
+})
+
+export const getTopListAction = (idx) => {
+  return dispatch => {
+    getTopList(idx).then(res => {
+      switch (idx) {
+        case 0:
+          dispatch(changeUpRankingAction(res));
+          break;
+        case 2:
+          dispatch(changeNewRankingAction(res));
+          break;
+        case 3:
+          dispatch(changeOriginRankingAction(res));
+          break;
+        default:
+      }
+    });
+  }
+}
